fc0500eeb7b9e7324570501c85c60232f8b91aad
Bloodmallet <kruse.peter.1990@gmail.com>
2018-03-27 14:02:31 +0200

    add basefile for settings

---
+++ bloodytools/settings.py
@@ -0,0 +1,11 @@
+# Profile setttings
+tier = "21"  # number or PR (PreRaid)
+fight_styles = ["patchwerk", "beastlord"]
+
+# Simulation settings
+executable = "../../SimulationCraft/simc.exe"
+threads = "8"
+profileset_work_threads = "2"
+
+# General Bloodytools Settings - you usually don't need to touch these
+debug = True
