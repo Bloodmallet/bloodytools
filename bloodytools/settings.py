46fcbe40b0f3b18307ee918f20a810a009bb6083
Bloodmallet <kruse.peter.1990@gmail.com>
2018-07-15 22:59:53 +0200

    add write_humanreadable_secondary_distribution_file to settings

--- bloodytools/settings.py
+++ bloodytools/settings.py
@@ -7,7 +7,7 @@
 # General setttings
 fight_styles = [
   "patchwerk",
-  #"beastlord"
+  "hecticaddcleave"
 ]
 tier = "PR"  # number or PR (PreRaid)
 wow_class_spec_list = []  # leave empty to simulate all
@@ -17,10 +17,10 @@ wow_class_spec_list = [("shaman", "elemental")]
 ##
 # SimulationCraft
 executable = "../../SimulationCraft_BfA/simc.exe"
-iterations = "75000"
+iterations = "250000"
 profileset_work_threads = "2"
 ptr = "0"
-simc_hash = "dec9b12600b28a81a11eef118b677116b9af4955"
+simc_hash = "c976e4e37ef5ed098155cebbb89012d85d5c3de7"
 target_error = "0.4"
 threads = "8"
 
@@ -30,7 +30,7 @@ enable_race_simulations = False
 
 ##
 # Trinket simulations
-enable_trinket_simulations = True
+enable_trinket_simulations = False
 ilevel_step = 10  # ilevel_step is used to determine the size of each itemlevel step taken to max_ilevel
 max_ilevel = 355  # max_itemlevel determines the upper border of steps taken
 min_ilevel = 300  # min_ilevel is used to determine the first simulated itemlevel
@@ -38,19 +38,22 @@ min_ilevel = 300  # min_ilevel is used to determine the first simulated itemleve
 
 ##
 # Secondary distributions
-enable_secondary_distributions_simulations = False
+enable_secondary_distributions_simulations = True
 talent_list = {
 }  # if no list is provided for a class-spec, all dps talent combinations will be run. If you want to only sim the base profiles, set 'talent_permutations' to False
 # talent_list = {
-#   ("shaman", "elemental"): ["1111111", "2222222"],
-# } # example for a talent list for Elemental Shamans
+#   ("shaman", "elemental"): [
+#     "2302023",
+#   ],
+# }  # example for a talent list for Elemental Shamans
 talent_permutations = True  # set to False, to sim only the base profile talent combinations
 
 ##
 # Developement setting - you usually don't need to touch these
-debug = False
+debug = True
 use_own_threading = False
 use_raidbots = False
+write_humanreadable_secondary_distribution_file = True
 try:
   from apikey import apikey
 except Exception:
