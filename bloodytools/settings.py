d807e411e9cd5024dee76b5fbc9c174b95703243
Bloodmallet <kruse.peter.1990@gmail.com>
2018-07-29 23:37:47 +0200

    enable all calculations

--- bloodytools/settings.py
+++ bloodytools/settings.py
@@ -7,43 +7,43 @@
 # General setttings
 fight_styles = [
   "patchwerk",
-  # "hecticaddcleave"
+  "hecticaddcleave"
   #"beastlord"
 ]
 tier = "21"  # number or PR (PreRaid)
 wow_class_spec_list = []  # leave empty to simulate all
 # wow_class_spec_list = [("shaman", "elemental"), ("mage", "frost")] # example for a specific list
 wow_class_spec_list = [
-  # ("death_knight", "blood"),
-  # ("death_knight", "frost"),
-  # ("death_knight", "unholy"),
-  # ("demon_hunter", "havoc"),
-  # ("demon_hunter", "vengeance"),
-  # ("druid", "balance"),
-  # ("druid", "feral"),
-  # ("druid", "guardian"),
-  # ("hunter", "beast_mastery"),
-  # ("hunter", "marksmanship"),
-  # ("hunter", "survival"),
-  # ("mage", "arcane"),
-  # ("mage", "fire"),
-  # ("mage", "frost"),
-  # ("monk", "brewmaster"),
-  # ("monk", "windwalker"),
-  # ("paladin", "protection"),
-  # ("paladin", "retribution"),
-  # ("priest", "shadow"),
-  # ("rogue", "assassination"),
-  # ("rogue", "outlaw"),
-  # ("rogue", "subtlety"),
+  ("death_knight", "blood"),
+  ("death_knight", "frost"),
+  ("death_knight", "unholy"),
+  ("demon_hunter", "havoc"),
+  ("demon_hunter", "vengeance"),
+  ("druid", "balance"),
+  ("druid", "feral"),
+  ("druid", "guardian"),
+  ("hunter", "beast_mastery"),
+  ("hunter", "marksmanship"),
+  ("hunter", "survival"),
+  ("mage", "arcane"),
+  ("mage", "fire"),
+  ("mage", "frost"),
+  ("monk", "brewmaster"),
+  ("monk", "windwalker"),
+  ("paladin", "protection"),
+  ("paladin", "retribution"),
+  ("priest", "shadow"),
+  ("rogue", "assassination"),
+  ("rogue", "outlaw"),
+  ("rogue", "subtlety"),
   ("shaman", "elemental"),
-  # ("shaman", "enhancement"),
-  # ("warlock", "affliction"),
-  # ("warlock", "demonology"),
-  # ("warlock", "destruction"),
-  # ("warrior", "arms"),
-  # ("warrior", "fury"),
-  # ("warrior", "protection"),
+  ("shaman", "enhancement"),
+  ("warlock", "affliction"),
+  ("warlock", "demonology"),
+  ("warlock", "destruction"),
+  ("warrior", "arms"),
+  ("warrior", "fury"),
+  ("warrior", "protection"),
 ]
 
 ##
@@ -52,17 +52,17 @@ executable = "../../SimulationCraft_BfA/simc.exe"
 iterations = "250000"
 profileset_work_threads = "2"
 ptr = "0"
-simc_hash = "183d98d9577d62ecd755cb59022cb275dd270acf"
+simc_hash = "ae005ee6c7554f56dc7deeb227a5f68de612e148"
 target_error = "0.2"
 threads = "8"
 
 ###############################################################################
 # Race simulations
-enable_race_simulations = False
+enable_race_simulations = True
 
 ###############################################################################
 # Trinket simulations
-enable_trinket_simulations = False
+enable_trinket_simulations = True
 ilevel_step = 10  # ilevel_step is used to determine the size of each itemlevel step taken to max_ilevel
 max_ilevel = 280  # max_itemlevel determines the upper border of steps taken
 min_ilevel = 210  # min_ilevel is used to determine the first simulated itemlevel
@@ -70,7 +70,7 @@ min_ilevel = 210  # min_ilevel is used to determine the first simulated itemleve
 
 ###############################################################################
 # Secondary distributions
-enable_secondary_distributions_simulations = False
+enable_secondary_distributions_simulations = True
 secondary_distributions_step_size = 10
 talent_list = {
 }  # if no list is provided for a class-spec, all dps talent combinations will be run. If you want to only sim the base profiles, set 'talent_permutations' to False
@@ -95,10 +95,10 @@ azerite_trait_ilevels = [ # determines the itemlevel used to sim the traits
 
 ###############################################################################
 # Development setting - you usually don't want to touch these
-debug = True
+debug = False
 use_own_threading = False
 use_raidbots = False
-write_humanreadable_secondary_distribution_file = True
+write_humanreadable_secondary_distribution_file = False
 lua_trinket_export = False
 try:
   from apikey import apikey
