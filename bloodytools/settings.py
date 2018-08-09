8994fa6225531dfd13342e85c34188489f537d10
Bloodmallet <kruse.peter.1990@gmail.com>
2018-08-09 20:29:27 +0200

    update settings to bfa values

--- bloodytools/settings.py
+++ bloodytools/settings.py
@@ -1,44 +1,44 @@
 """Settings for bloodytools
 
-  Look for the matching paragraphs of what you want to do. And change settings responsibly. If anything breaks too hard, just reload the settingsfile from the repository.
+  Look for the matching paragraphs of what you want to do. Change settings responsibly. If anything breaks too hard, just reload the settings-file from the repository.
 """
 
 ##
 # General setttings
-tier = "21"  # number or PR (PreRaid)
+tier = "PR"  # number or PR (PreRaid)
 wow_class_spec_list = []  # leave empty to simulate all
 # wow_class_spec_list = [("shaman", "elemental"), ("mage", "frost")] # example for a specific list
 wow_class_spec_list = [
-  # ("death_knight", "blood"),
-  # ("death_knight", "frost"),
-  # ("death_knight", "unholy"),
-  # ("demon_hunter", "havoc"),
-  # ("demon_hunter", "vengeance"),
-  # ("druid", "balance"),
-  # ("druid", "feral"),
-  # ("druid", "guardian"),
-  # ("hunter", "beast_mastery"),
-  # ("hunter", "marksmanship"),
-  # ("hunter", "survival"),
-  # ("mage", "arcane"),
-  # ("mage", "fire"),
-  # ("mage", "frost"),
-  # ("monk", "brewmaster"),
-  # ("monk", "windwalker"),
-  # ("paladin", "protection"),
-  # ("paladin", "retribution"),
-  # ("priest", "shadow"),
-  # ("rogue", "assassination"),
-  # ("rogue", "outlaw"),
-  # ("rogue", "subtlety"),
+  ("death_knight", "blood"),
+  ("death_knight", "frost"),
+  ("death_knight", "unholy"),
+  ("demon_hunter", "havoc"),
+  ("demon_hunter", "vengeance"),
+  ("druid", "balance"),
+  ("druid", "feral"),
+  ("druid", "guardian"),
+  ("hunter", "beast_mastery"),
+  ("hunter", "marksmanship"),
+  ("hunter", "survival"),
+  ("mage", "arcane"),
+  ("mage", "fire"),
+  ("mage", "frost"),
+  ("monk", "brewmaster"),
+  ("monk", "windwalker"),
+  ("paladin", "protection"),
+  ("paladin", "retribution"),
+  ("priest", "shadow"),
+  ("rogue", "assassination"),
+  ("rogue", "outlaw"),
+  ("rogue", "subtlety"),
   ("shaman", "elemental"),
-  # ("shaman", "enhancement"),
-  # ("warlock", "affliction"),
-  # ("warlock", "demonology"),
-  # ("warlock", "destruction"),
-  # ("warrior", "arms"),
-  # ("warrior", "fury"),
-  # ("warrior", "protection"),
+  ("shaman", "enhancement"),
+  ("warlock", "affliction"),
+  ("warlock", "demonology"),
+  ("warlock", "destruction"),
+  ("warrior", "arms"),
+  ("warrior", "fury"),
+  ("warrior", "protection"),
 ]
 
 ##
@@ -49,33 +49,33 @@ fight_styles = [
   "hecticaddcleave"
   #"beastlord"
 ]
-iterations = "250000"
+iterations = "20000" # sane value, should be enough for 0.2
 profileset_work_threads = "2"
 ptr = "0"
-simc_hash = "992db1b5638e5d2b14b9f17d6d3b0308a0cf9af4"
+simc_hash = "d74cad902abf9da07f74bc874767a3a702ac4af7"
 target_error = {
   "patchwerk": "0.2",
-  "hecticaddcleave": "0.4",
+  "hecticaddcleave": "0.2",
   "beastlord": "0.4"
 }
 threads = "8"
 
 ###############################################################################
 # Race simulations
-enable_race_simulations = False
+enable_race_simulations = True
 
 ###############################################################################
 # Trinket simulations
-enable_trinket_simulations = False
-ilevel_step = 10  # ilevel_step is used to determine the size of each itemlevel step taken to max_ilevel
-max_ilevel = 280  # max_itemlevel determines the upper border of steps taken
-min_ilevel = 210  # min_ilevel is used to determine the first simulated itemlevel
+enable_trinket_simulations = True
+ilevel_step = 15  # ilevel_step is used to determine the size of each itemlevel step taken to max_ilevel
+max_ilevel = 355  # max_itemlevel determines the upper border of steps taken
+min_ilevel = 325  # min_ilevel is used to determine the first simulated itemlevel and second trinket (vers stat stick)
 # example: min 300, max 325, step 10, resulting simulated ilevels: 300, 310, 320
 
 ###############################################################################
 # Secondary distributions
-enable_secondary_distributions_simulations = False
-secondary_distributions_step_size = 10
+enable_secondary_distributions_simulations = True
+secondary_distributions_step_size = 10 # in percent of full available secondary sum
 talent_list = {
 }  # if no list is provided for a class-spec, all dps talent combinations will be run. If you want to only sim the base profiles, set 'talent_permutations' to False
 # talent_list = {
@@ -88,7 +88,7 @@ talent_permutations = False  # set to False, to sim only the base profile talent
 
 ###############################################################################
 # Azerite traits
-enable_azerite_trait_simulations = False
+enable_azerite_trait_simulations = True
 azerite_trait_ilevels = [ # determines the itemlevel used to sim the traits
   "340",
   "355",
@@ -99,7 +99,7 @@ azerite_trait_ilevels = [ # determines the itemlevel used to sim the traits
 
 ###############################################################################
 # Gear path
-enable_gear_path = True
+enable_gear_path = False
 step_size = 50
 start_value = 50
 
