6cf092bd0afc77badfb31bee826707b4bcdb945a
Bloodmallet(EU) <kruse.peter.1990@gmail.com>
2020-09-06 19:56:29 +0200

    disable everything but trinket_simulations

--- bloodytools/main.py
+++ bloodytools/main.py
@@ -29,16 +29,15 @@ import threading
 import time
 
 from bloodytools import settings
-from bloodytools.simulations.gear_path_simulation import gear_path_simulation
-from bloodytools.simulations.race_simulation import race_simulation
-from bloodytools.simulations.secondary_distribution_simulation import secondary_distribution_simulation
+# from bloodytools.simulations.gear_path_simulation import gear_path_simulation
+# from bloodytools.simulations.race_simulation import race_simulation
+# from bloodytools.simulations.secondary_distribution_simulation import secondary_distribution_simulation
 from bloodytools.simulations.trinket_simulation import trinket_simulation
-from bloodytools.simulations.talent_worth_simulation import talent_worth_simulation
+# from bloodytools.simulations.talent_worth_simulation import talent_worth_simulation
 from bloodytools.utils.utils import get_simc_hash
 from bloodytools.utils.utils import arg_parse_config
 from bloodytools.utils.utils import logger_config
-from simc_support import wow_lib
-
+from simc_support.game_data.WowSpec import WOWSPECS, get_wow_spec
 
 logger = logging.getLogger(__name__)
 
@@ -65,7 +64,7 @@ def main(args: object):
 
         # single sim will always use all cores unless --threads is defined
         settings.threads = ""
-        settings.wow_class_spec_list = [(wow_class.lower(), wow_spec.lower())]
+        settings.wow_class_spec_list = [get_wow_spec(wow_class, wow_spec), ]
         settings.fight_styles = [
             fight_style,
         ]
@@ -74,33 +73,22 @@ def main(args: object):
         settings.enable_race_simulations = False
         settings.enable_trinket_simulations = False
         settings.enable_secondary_distributions_simulations = False
-        settings.enable_azerite_trait_simulations = False
         settings.enable_gear_path = False
         settings.enable_talent_worth_simulations = False
-        settings.enable_azerite_essence_simulations = False
-        settings.enable_azerite_essence_combination_simulations = False
-        settings.enable_corruption_simulations = False
 
         # set dev options
         settings.use_own_threading = False
         settings.use_raidbots = False
 
-        if simulation_type == "races":
-            settings.enable_race_simulations = True
-        elif simulation_type == "trinkets":
+        # TODO: re-enable other simulation types
+        # if simulation_type == "races":
+        #     settings.enable_race_simulations = True
+        if simulation_type == "trinkets":
             settings.enable_trinket_simulations = True
-        elif simulation_type == "azerite_traits":
-            settings.enable_azerite_trait_simulations = True
-        elif simulation_type == "secondary_distributions":
-            settings.enable_secondary_distributions_simulations = True
-        elif simulation_type == "talent_worth":
-            settings.enable_talent_worth_simulations = True
-        elif simulation_type == "essences":
-            settings.enable_azerite_essence_simulations = True
-        elif simulation_type == "essence_combinations":
-            settings.enable_azerite_essence_combination_simulations = True
-        elif simulation_type == "corruptions":
-            settings.enable_corruption_simulations = True
+        # elif simulation_type == "secondary_distributions":
+        #     settings.enable_secondary_distributions_simulations = True
+        # elif simulation_type == "talent_worth":
+        #     settings.enable_talent_worth_simulations = True
 
     # set new executable path if provided
     if args.executable:
@@ -149,27 +137,28 @@ def main(args: object):
 
     # empty class-spec list? great, we'll run all class-spec combinations
     if not settings.wow_class_spec_list:
-        settings.wow_class_spec_list = wow_lib.get_classes_specs()
+        settings.wow_class_spec_list = WOWSPECS
 
     # list of all active threads. when empty, terminate tool
     thread_list = []
 
+    # TODO: re-enable other simulation types
     # trigger race simulations
-    if settings.enable_race_simulations:
-        if not settings.use_own_threading:
-            logger.info("Starting Race simulations.")
+    # if settings.enable_race_simulations:
+    #     if not settings.use_own_threading:
+    #         logger.info("Starting Race simulations.")
 
-        if settings.use_own_threading:
-            race_thread = threading.Thread(
-                name="Race Thread", target=race_simulation, args=(settings.wow_class_spec_list, settings)
-            )
-            thread_list.append(race_thread)
-            race_thread.start()
-        else:
-            race_simulation(settings.wow_class_spec_list, settings)
+    #     if settings.use_own_threading:
+    #         race_thread = threading.Thread(
+    #             name="Race Thread", target=race_simulation, args=(settings.wow_class_spec_list, settings)
+    #         )
+    #         thread_list.append(race_thread)
+    #         race_thread.start()
+    #     else:
+    #         race_simulation(settings.wow_class_spec_list, settings)
 
-        if not settings.use_own_threading:
-            logger.info("Race simulations finished.")
+    #     if not settings.use_own_threading:
+    #         logger.info("Race simulations finished.")
 
     # trigger trinket simulations
     if settings.enable_trinket_simulations:
@@ -188,63 +177,66 @@ def main(args: object):
         if not settings.use_own_threading:
             logger.info("Trinket simulations finished.")
 
+    # TODO: re-enable other simulation types
     # trigger secondary distributions
-    if settings.enable_secondary_distributions_simulations:
+    # if settings.enable_secondary_distributions_simulations:
 
-        if not settings.use_own_threading:
-            logger.info("Starting Secondary Distribtion simulations.")
+    #     if not settings.use_own_threading:
+    #         logger.info("Starting Secondary Distribtion simulations.")
 
-        if settings.use_own_threading:
-            secondary_distribution_thread = threading.Thread(
-                name="Secondary Distribution Thread",
-                target=secondary_distribution_simulation,
-                args=(settings,)
-            )
-            thread_list.append(secondary_distribution_thread)
-            secondary_distribution_thread.start()
-        else:
-            secondary_distribution_simulation(settings)
+    #     if settings.use_own_threading:
+    #         secondary_distribution_thread = threading.Thread(
+    #             name="Secondary Distribution Thread",
+    #             target=secondary_distribution_simulation,
+    #             args=(settings,)
+    #         )
+    #         thread_list.append(secondary_distribution_thread)
+    #         secondary_distribution_thread.start()
+    #     else:
+    #         secondary_distribution_simulation(settings)
 
-        if not settings.use_own_threading:
-            logger.info("Secondary Distribution simulations finished.")
+    #     if not settings.use_own_threading:
+    #         logger.info("Secondary Distribution simulations finished.")
 
+    # TODO: re-enable other simulation types
     # trigger gear path simulations
-    if settings.enable_gear_path:
-        if not settings.use_own_threading:
-            logger.info("Gear Path simulations start.")
+    # if settings.enable_gear_path:
+    #     if not settings.use_own_threading:
+    #         logger.info("Gear Path simulations start.")
 
-        if settings.use_own_threading:
-            gearing_path_thread = threading.Thread(
-                name="Gear Path Thread",
-                target=gear_path_simulation,
-                args=(settings.wow_class_spec_list, settings)
-            )
-            thread_list.append(gearing_path_thread)
-            gearing_path_thread.start()
-        else:
-            gear_path_simulation(settings.wow_class_spec_list, settings)
+    #     if settings.use_own_threading:
+    #         gearing_path_thread = threading.Thread(
+    #             name="Gear Path Thread",
+    #             target=gear_path_simulation,
+    #             args=(settings.wow_class_spec_list, settings)
+    #         )
+    #         thread_list.append(gearing_path_thread)
+    #         gearing_path_thread.start()
+    #     else:
+    #         gear_path_simulation(settings.wow_class_spec_list, settings)
 
-        if not settings.use_own_threading:
-            logger.info("Gear Path simulations end.")
+    #     if not settings.use_own_threading:
+    #         logger.info("Gear Path simulations end.")
 
+    # TODO: re-enable other simulation types
     # trigger talent worth simulations
-    if settings.enable_talent_worth_simulations:
-        if not settings.use_own_threading:
-            logger.info("Talent Worth simulations start.")
+    # if settings.enable_talent_worth_simulations:
+    #     if not settings.use_own_threading:
+    #         logger.info("Talent Worth simulations start.")
 
-        if settings.use_own_threading:
-            talent_worth_thread = threading.Thread(
-                name="Talent Worth Thread",
-                target=talent_worth_simulation,
-                args=(settings.wow_class_spec_list, settings)
-            )
-            thread_list.append(talent_worth_thread)
-            talent_worth_thread.start()
-        else:
-            talent_worth_simulation(settings.wow_class_spec_list, settings)
+    #     if settings.use_own_threading:
+    #         talent_worth_thread = threading.Thread(
+    #             name="Talent Worth Thread",
+    #             target=talent_worth_simulation,
+    #             args=(settings.wow_class_spec_list, settings)
+    #         )
+    #         thread_list.append(talent_worth_thread)
+    #         talent_worth_thread.start()
+    #     else:
+    #         talent_worth_simulation(settings.wow_class_spec_list, settings)
 
-        if not settings.use_own_threading:
-            logger.info("Talent Worth simulations end.")
+    #     if not settings.use_own_threading:
+    #         logger.info("Talent Worth simulations end.")
 
     while thread_list:
         time.sleep(1)
