1eb085b755338eca9ebff3fdd3ee8c0e715b4665
Bloodmallet(EU) <kruse.peter.1990@gmail.com>
2020-10-29 21:18:44 +0100

    allow quotes in input

--- bloodytools/utils/utils.py
+++ bloodytools/utils/utils.py
@@ -116,7 +116,9 @@ def extract_profile(path: str, wow_class: WowClass, profile: dict = None) -> dic
     unified_slot_names = {"shoulder": "shoulders", "wrist": "wrists"}
     pattern_slots = {}
     for element in item_slots:
-        pattern_slots[element] = re.compile("^{}=([a-z0-9_=,/:.]*)$".format(element))
+        pattern_slots[element] = re.compile(
+            '^{}="?([a-z0-9_=,/:.]*)"?$'.format(element)
+        )
 
     # prepare regex for item defining attributes
     item_elements = [
@@ -132,7 +134,7 @@ def extract_profile(path: str, wow_class: WowClass, profile: dict = None) -> dic
     pattern_element = {}
     # don't recompile this for each slot
     for element in item_elements:
-        pattern_element[element] = re.compile(",{}=([a-z0-9_/:]*)".format(element))
+        pattern_element[element] = re.compile(',{}="?([a-z0-9_/:]*)"?'.format(element))
 
     # prepare regex for character defining information. like spec
     character_specifics = [
@@ -148,7 +150,9 @@ def extract_profile(path: str, wow_class: WowClass, profile: dict = None) -> dic
     ]
     pattern_specifics = {}
     for element in character_specifics:
-        pattern_specifics[element] = re.compile("^{}=([a-z0-9_./:]*)$".format(element))
+        pattern_specifics[element] = re.compile(
+            '^{}="?([a-z0-9_./:]*)"?$'.format(element)
+        )
 
     with open(path, "r") as f:
         for line in f:
