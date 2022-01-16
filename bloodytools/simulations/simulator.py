14766a63c6be8f8d584c970c25e77d52f0b47062
Bloodmallet <bloodmalleteu@gmail.com>
2022-01-16 10:47:15 +0100

    remove forgotten import

--- bloodytools/simulations/simulator.py
+++ bloodytools/simulations/simulator.py
@@ -7,7 +7,6 @@ import typing
 
 from bloodytools.utils.config import Config
 from bloodytools.utils.data_type import DataType
-from bloodytools.utils.output import OutputJson, SimulatedData
 from bloodytools.utils.simulation_objects import Simulation_Group
 from bloodytools.utils.utils import create_base_json_dict
 from simc_support.game_data.WowSpec import WowSpec
