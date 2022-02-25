87be75f978c1372883f4c0dc4ce94c16477aca5a
Bloodmallet <bloodmalleteu@gmail.com>
2022-02-25 14:17:59 +0100

    enable using covenant profiles if present

--- bloodytools/simulations/tier_set_simulator.py
+++ bloodytools/simulations/tier_set_simulator.py
@@ -1,5 +1,6 @@
 from bloodytools.utils.simulation_objects import Simulation_Data, Simulation_Group
 from .simulator import Simulator
+from simc_support.game_data.Covenant import get_covenant
 
 
 class TierSetSimulator(Simulator):
@@ -26,34 +27,48 @@ class TierSetSimulator(Simulator):
         }
 
         for tier, simc_input in tier_mapping.items():
-            spec = str(self.wow_spec).replace("_", " ")
-            data = Simulation_Data(
-                name=self.profile_split_character().join([spec, tier]),
-                simc_arguments=simc_input,
-                fight_style=self.fight_style,
-                profile=data_dict["profile"],
-                target_error=self.settings.target_error.get(self.fight_style, "0.1"),
-                ptr=self.settings.ptr,
-                default_actions=self.settings.default_actions,
-                executable=self.settings.executable,
-                iterations=self.settings.iterations,
-            )
+            for covenant_name, covenant_profile in data_dict[
+                "covenant_profiles"
+            ].items():
+                covenant = get_covenant(name=covenant_name)
+                data = Simulation_Data(
+                    name=self.profile_split_character().join(
+                        [covenant.full_name, tier]
+                    ),
+                    simc_arguments=simc_input,
+                    fight_style=self.fight_style,
+                    profile=covenant_profile,
+                    target_error=self.settings.target_error.get(
+                        self.fight_style, "0.1"
+                    ),
+                    ptr=self.settings.ptr,
+                    default_actions=self.settings.default_actions,
+                    executable=self.settings.executable,
+                    iterations=self.settings.iterations,
+                )
 
-            if tier == "no tier":
-                custom_apl = None
-                if self.settings.custom_apl:
-                    with open("custom_apl.txt") as f:
-                        custom_apl = f.read()
-                if custom_apl:
-                    data.simc_arguments.append("# custom_apl")
-                    data.simc_arguments.append(custom_apl)
+                if len(simulation_group.profiles) == 0:
+                    custom_apl = None
+                    if self.settings.custom_apl:
+                        with open("custom_apl.txt") as f:
+                            custom_apl = f.read()
+                    if custom_apl:
+                        data.simc_arguments.append("# custom_apl")
+                        data.simc_arguments.append(custom_apl)
 
-                custom_fight_style = None
-                if self.settings.custom_fight_style:
-                    with open("custom_fight_style.txt") as f:
-                        custom_fight_style = f.read()
-                if custom_fight_style:
-                    data.simc_arguments.append("# custom_fight_style")
-                    data.simc_arguments.append(custom_fight_style)
+                    custom_fight_style = None
+                    if self.settings.custom_fight_style:
+                        with open("custom_fight_style.txt") as f:
+                            custom_fight_style = f.read()
+                    if custom_fight_style:
+                        data.simc_arguments.append("# custom_fight_style")
+                        data.simc_arguments.append(custom_fight_style)
 
-            simulation_group.add(data)
+                simulation_group.add(data)
+
+    def post_processing(self, data_dict: dict) -> dict:
+        data_dict = super().post_processing(data_dict)
+
+        self.create_sorted_key_key_value_data(data_dict)
+
+        return data_dict
