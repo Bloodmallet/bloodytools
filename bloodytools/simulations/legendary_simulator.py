fb3350424a34e0c900c395a4b6077dbc6841a075
Bloodmallet <bloodmalleteu@gmail.com>
2022-04-02 12:05:19 +0200

    stop moving legendary effects around

--- bloodytools/simulations/legendary_simulator.py
+++ bloodytools/simulations/legendary_simulator.py
@@ -6,7 +6,7 @@ import yaml
 from bloodytools.simulations.simulator import Simulator
 from bloodytools.utils.simulation_objects import Simulation_Data, Simulation_Group
 from bloodytools.utils.utils import EmptyFileError, extract_profile
-from simc_support.game_data.Covenant import Covenant
+from simc_support.game_data.Covenant import Covenant, get_covenant
 from simc_support.game_data.Legendary import (
     LEGENDARIES,
     Legendary,
@@ -47,9 +47,37 @@ def _load_special_cases() -> typing.Dict[WowSpec, typing.List[dict]]:
     return SPECIAL_CASES
 
 
-def remove_legendary_bonus_ids(
-    profile: dict, unwanted_ids: typing.Iterable[int]
+def set_covenant_legendary(
+    profile: dict, covenant: Covenant, legendaries: typing.List[Legendary]
 ) -> dict:
+    """Instead of Unity set the specific covenant legendary bonus id
+
+    Args:
+        profile (dict): _description_
+        covenant (Covenant): _description_
+        legendaries (typing.List[Legendary]): _description_
+
+    Raises:
+        ValueError: _description_
+
+    Returns:
+        dict: _description_
+    """
+
+    wanted_legendary: typing.Optional[Legendary] = None
+    for l in legendaries:
+        if len(l.covenants) == 1 and l.covenants[0] == covenant:
+            wanted_legendary = l
+
+    if wanted_legendary is None:
+        raise ValueError(f"Couldn't find Legendary for covenant {covenant}.")
+
+    replace_legendary_bonus_ids = [
+        l.bonus_id
+        for l in legendaries
+        if len(l.covenants) == 1 or "unity" in l.full_name.lower()
+    ]
+
     for item in profile["items"]:
         item_bonus_ids: typing.List[str] = []
         try:
@@ -63,13 +91,47 @@ def remove_legendary_bonus_ids(
                 item_bonus_ids.append(b_id)
 
         if item_bonus_ids and any(item_bonus_ids):
-            filtered_item_bonus_ids = [
-                b_id for b_id in item_bonus_ids if not int(b_id) in unwanted_ids
-            ]
+            filtered_item_bonus_ids = []
+
+            for b_id in item_bonus_ids:
+                if not int(b_id) in replace_legendary_bonus_ids:
+                    filtered_item_bonus_ids.append(b_id)
+                else:
+                    filtered_item_bonus_ids.append(str(wanted_legendary.bonus_id))
 
             profile["items"][item]["bonus_id"] = "/".join(filtered_item_bonus_ids)
 
     return profile
+
+
+def remove_bonus_ids(
+    profile: dict, unwanted_ids: typing.Iterable[int]
+) -> typing.Tuple[dict, typing.List[str]]:
+    purged_slots: typing.List[str] = []
+    for item in profile["items"]:
+        item_bonus_ids: typing.List[str] = []
+        try:
+            tmp_item_bonus_ids = profile["items"][item]["bonus_id"].split("/")
+        except KeyError:
+            continue
+
+        for element in tmp_item_bonus_ids:
+            # the user could also use : to split bonus ids >.>
+            for b_id in element.split(":"):
+                item_bonus_ids.append(b_id)
+
+        if item_bonus_ids and any(item_bonus_ids):
+            filtered_item_bonus_ids = []
+
+            for b_id in item_bonus_ids:
+                if not int(b_id) in unwanted_ids:
+                    filtered_item_bonus_ids.append(b_id)
+                else:
+                    purged_slots.append(item)
+
+            profile["items"][item]["bonus_id"] = "/".join(filtered_item_bonus_ids)
+
+    return (profile, purged_slots)
 
 
 def get_legendary_special_case_name(
@@ -97,19 +159,28 @@ def _get_legendary(full_name: str) -> typing.Optional[Legendary]:
     return None
 
 
-def _get_head_with_legendary(profile: dict) -> str:
-    item_information = profile["items"]["head"]
-    constructed_item = f"head=,id={item_information['id']}"
+def _create_item_for_legendary(profile: dict, slot: str) -> str:
+    """Only the legendary ID needs to be added.
+
+    Args:
+        profile (dict): _description_
+        slot (str): _description_
+
+    Returns:
+        str: _description_
+    """
+    item_information = profile["items"][slot]
+    constructed_item = f"{slot}=,id={item_information['id']}"
     for key in item_information.keys():
         if key not in ("id", "bonus_id"):
             constructed_item += f",{key}={item_information[key]}"
 
     try:
-        head = constructed_item + f",bonus_id={item_information['bonus_id']}/"
+        final_item = constructed_item + f",bonus_id={item_information['bonus_id']}/"
     except KeyError:
-        head = constructed_item + f",bonus_id="
+        final_item = constructed_item + f",bonus_id="
 
-    return head
+    return final_item
 
 
 class LegendarySimulator(Simulator):
@@ -135,15 +206,38 @@ class LegendarySimulator(Simulator):
         ]
         self.legendaries = filtered_legendaries
 
-        all_legendary_bonus_ids = [legendary.bonus_id for legendary in legendaries]
-        data_dict["profile"] = remove_legendary_bonus_ids(
-            data_dict["profile"], all_legendary_bonus_ids
+        # handle unity/covenant legendaries
+        cov = get_covenant(name=data_dict["profile"]["character"]["covenant"])
+        data_dict["profile"] = set_covenant_legendary(
+            data_dict["profile"], cov, legendaries
         )
-
         for profile in data_dict["covenant_profiles"]:
-            data_dict["covenant_profiles"][profile] = remove_legendary_bonus_ids(
-                data_dict["covenant_profiles"][profile], all_legendary_bonus_ids
+            cov = get_covenant(
+                name=data_dict["covenant_profiles"][profile]["character"]["covenant"]
             )
+            data_dict["covenant_profiles"][profile] = set_covenant_legendary(
+                data_dict["covenant_profiles"][profile], cov, legendaries
+            )
+
+        # remove non-covenant legendaries
+        self.legendary_slots = {}
+        non_covenant_legendary_bonus_ids = [
+            legendary.bonus_id
+            for legendary in legendaries
+            if len(legendary.covenants) != 1
+            and "unity" not in legendary.full_name.lower()
+        ]
+        for profile in data_dict["covenant_profiles"]:
+            data_dict["covenant_profiles"][profile], purged_slots = remove_bonus_ids(
+                data_dict["covenant_profiles"][profile],
+                non_covenant_legendary_bonus_ids,
+            )
+            if len(purged_slots) != 1:
+                raise ValueError(f"Slots {purged_slots} were purged. Expected 1.")
+            else:
+                self.legendary_slots[
+                    data_dict["covenant_profiles"][profile]["character"]["covenant"]
+                ] = purged_slots[0]
 
         data_dict["spell_ids"] = {}
         for legendary in legendaries:
@@ -183,16 +277,9 @@ class LegendarySimulator(Simulator):
         self, simulation_group: Simulation_Group, data_dict: dict
     ) -> None:
 
-        covenant_item = _get_head_with_legendary(data_dict["profile"])
         covenant_simc_name = data_dict["profile"]["character"]["covenant"]
-        covenant_legendary = _get_covenant_legendary(
-            covenant_simc_name, self.legendaries
-        )
-        if covenant_legendary:
-            covenant_item += f"/{covenant_legendary.bonus_id}"
-
-        logger.debug(
-            f"Baseline profile is '{covenant_simc_name}', legendary is '{covenant_legendary}'"
+        covenant_item = _create_item_for_legendary(
+            data_dict["profile"], self.legendary_slots[covenant_simc_name]
         )
 
         simulation_data = Simulation_Data(
@@ -227,19 +314,11 @@ class LegendarySimulator(Simulator):
         SPECIAL_CASES = _load_special_cases()
 
         for covenant_name in data_dict["covenant_profiles"]:
-            covenant_item = _get_head_with_legendary(
-                data_dict["covenant_profiles"][covenant_name]
+            covenant_item = _create_item_for_legendary(
+                data_dict["covenant_profiles"][covenant_name],
+                self.legendary_slots[covenant_name],
             )
             covenant_simc_name = covenant_name
-            covenant_legendary = _get_covenant_legendary(
-                covenant_simc_name, self.legendaries
-            )
-            if covenant_legendary:
-                covenant_item += f"/{covenant_legendary.bonus_id}"
-
-            logger.debug(
-                f"{covenant_name} profile is '{covenant_simc_name}', legendary is '{covenant_legendary}'"
-            )
 
             simulation_data = Simulation_Data(
                 name=f"{{{covenant_name}}}",
@@ -256,9 +335,10 @@ class LegendarySimulator(Simulator):
 
         for legendary in self.legendaries:
 
-            if len(legendary.covenants) == 1:
+            if len(legendary.covenants) == 1 or "unity" in legendary.full_name.lower():
                 # single covenant legendary simulations aren't necessary
                 continue
+
             covenant: Covenant
             for covenant in legendary.covenants:
                 if (
@@ -274,13 +354,10 @@ class LegendarySimulator(Simulator):
                 elif covenant.simc_name in data_dict["covenant_profiles"]:
                     profile = data_dict["covenant_profiles"][covenant.simc_name]
 
-                covenant_item = _get_head_with_legendary(profile)
-                covenant_simc_name = covenant.simc_name
-                covenant_legendary = _get_covenant_legendary(
-                    covenant_simc_name, self.legendaries
+                covenant_item = _create_item_for_legendary(
+                    profile, self.legendary_slots[covenant.simc_name]
                 )
-                if covenant_legendary:
-                    covenant_item += f"/{covenant_legendary.bonus_id}"
+                covenant_simc_name = covenant.simc_name
                 covenant_item += f"/{legendary.bonus_id}"
 
                 simulation_data = Simulation_Data(
