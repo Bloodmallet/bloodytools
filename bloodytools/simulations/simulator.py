b70e45a35e6d900cc2f31fe575afa1afe76d0ae5
Peter Kruse <kruse.peter.1990@gmail.com>
2023-10-08 13:39:33 +0200

    inform user about result location

--- bloodytools/simulations/simulator.py
+++ bloodytools/simulations/simulator.py
@@ -234,7 +234,7 @@ class Simulator(abc.ABC):
 
         file_name = f"{self.wow_spec.wow_class.simc_name}_{self.wow_spec.simc_name}_{self.fight_style.lower()}.json"
         full_path = os.path.join(path, file_name)
-        logger.debug(f"Writing data to {full_path}")
+        logger.info(f"Writing result to {full_path}")
         # write json to file
         with open(full_path, "w", encoding="utf-8") as f:
             f.write(
