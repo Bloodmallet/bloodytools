260a44806b66aa79eda6aa5d415bfa9019748734
Peter Kruse <kruse.peter.1990@gmail.com>
2026-04-08 08:07:27 +0200

    havoc also should use a primary stat pot to reduce impact on trinket sims

--- bloodytools/simulations/trinket_simulator.py
+++ bloodytools/simulations/trinket_simulator.py
@@ -412,7 +412,14 @@ class TrinketSimulator(Simulator):
             load_custom_apl=self.settings.custom_apl,
         )
 
-        if ("demon_hunter", "devourer") in self.settings.wow_class_spec_names:
+        # These specs use the flexible secondary stat pot by default. To
+        # properly utilize this they rely heavily on a specific secondary stat
+        # distribution. By replacing the trinkets this can screw data a lot.
+        # Therefore the current solution is to use a different but weaker pot.
+        if ("demon_hunter", "devourer") in self.settings.wow_class_spec_names or (
+            "demon_hunter",
+            "havoc",
+        ) in self.settings.wow_class_spec_names:
             simulation_data.simc_arguments.append("potion=lights_potential_2")
 
         simulation_group.add(simulation_data)
